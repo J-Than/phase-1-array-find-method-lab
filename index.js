5b05ff63ed4c0764d2e6ca806f7e00dd14ddc02f
Jarod Wilson <jaroddwilson@gmail.com>
2021-10-20 13:57:05 -0400

    commit tree 861e2af50d7b9a432de247d1cb0804204713cd9b

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let indexReference;
 
+function isWin(element, index){
+    indexReference = index;
+    return element.result === 'W';
+}
+
+function superbowlWin(dataArray){
+    dataArray.find(isWin);
+    return dataArray[indexReference].year;
+}
